678b849a088ce26d61e9d9e713822a2b206fa47a
gabrieltsc <bielterry@hotmail.com>
2024-01-18 14:58:48 -0300

    Adição da pasta de loop e repetição

---
+++ Loop_e_Repeticao/index.js
@@ -0,0 +1,9 @@
+let limite = 10;
+
+for (let i = 0; i < limite; i++) {
+    if (i%2 == 1) {
+        console.log(i + " é um número ímpar.")
+    } else {
+        console.log(i + " é um número par.")
+    }
+}
